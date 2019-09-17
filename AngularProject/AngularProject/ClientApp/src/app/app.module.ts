a577b4737fffd154d23d0847dc41936a5985b927
mittulmandhan <conanmittul@gmail.com>
2019-09-18 03:53:47 +0530

    home, signup, login and cart component added

--- AngularProject/AngularProject/ClientApp/src/app/app.module.ts
+++ AngularProject/AngularProject/ClientApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
