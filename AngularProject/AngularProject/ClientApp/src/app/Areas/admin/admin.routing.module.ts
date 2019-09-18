b63fda6cca17d4217b226c18d7d52f05980c65e5
mittulmandhan <conanmittul@gmail.com>
2019-09-18 05:59:43 +0530

    routing for admin module done

--- AngularProject/AngularProject/ClientApp/src/app/Areas/admin/admin.routing.module.ts
+++ AngularProject/AngularProject/ClientApp/src/app/Areas/admin/admin.routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
+    {path: '', component: DashboardComponent},
+    {path: 'products', component: ProductsComponent},
 
 ];
 
