880cb6ee92648f545fed3874742df6ee8540cd82
mittulmandhan <conanmittul@gmail.com>
2019-09-18 21:39:36 +0530

    user model added

---
+++ AngularProject/AngularProject/ClientApp/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class UserModel {
+    UserId: number;
+    Username: string;
+    Password: string;
+    ContactNo: string;
+    Address: string;
+    Name: string;
+    Token: string;
+    Roles: string[];
+}
