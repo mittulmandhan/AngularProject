8bf5af2579a92fbe954af364e131fa5046548725
mittulmandhan <conanmittul@gmail.com>
2019-09-19 19:59:56 +0530

    admin layout customized

--- AngularProject/AngularProject/ClientApp/src/app/Areas/admin/admin.module.ts
+++ AngularProject/AngularProject/ClientApp/src/app/Areas/admin/admin.module.ts
@@ -1,14 +1,13 @@
+import { AdminLayoutComponent } from './shared/layout/adminlayout.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductsComponent } from './products/products.component';
 import { AdminRoutingModule } from './admin.routing.module';
-import { LayoutComponent } from './shared/layout/layout.component';
-
 
 
 @NgModule({
-  declarations: [DashboardComponent, ProductsComponent, LayoutComponent],
+  declarations: [DashboardComponent, ProductsComponent, AdminLayoutComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
---
+++ AngularProject/AngularProject/ClientApp/src/app/Areas/admin/shared/layout/adminlayout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminlayout',
+  templateUrl: './adminlayout.component.html',
+  styles: []
+})
+export class AdminLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
