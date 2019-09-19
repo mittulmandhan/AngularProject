d42b011dec5db945993bb42d416b9c32af29c967
mittulmandhan <conanmittul@gmail.com>
2019-09-19 19:06:26 +0530

    user component added

--- AngularProject/AngularProject/ClientApp/src/app/Areas/user/user.module.ts
+++ AngularProject/AngularProject/ClientApp/src/app/Areas/user/user.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserRoutingModule } from './user.routing.module';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, LayoutComponent],
   imports: [
     CommonModule,
     UserRoutingModule
