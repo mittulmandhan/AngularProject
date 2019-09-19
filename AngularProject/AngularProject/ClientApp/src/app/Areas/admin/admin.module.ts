6dd271c4edc88db6312645b78d3c761d8bd5e3e4
mittulmandhan <conanmittul@gmail.com>
2019-09-19 19:05:21 +0530

    admin layout component added

--- AngularProject/AngularProject/ClientApp/src/app/Areas/admin/admin.module.ts
+++ AngularProject/AngularProject/ClientApp/src/app/Areas/admin/admin.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductsComponent } from './products/products.component';
 import { AdminRoutingModule } from './admin.routing.module';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, ProductsComponent],
+  declarations: [DashboardComponent, ProductsComponent, LayoutComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
