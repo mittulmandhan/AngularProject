b3b2ba6d14fbc24107755914bbc69c9cb9c9da75
mittulmandhan <conanmittul@gmail.com>
2019-09-22 02:32:06 +0530

    some properties added for payment gateway and backend call

--- AngularProject/AngularProject/ClientApp/src/environments/environment.prod.ts
+++ AngularProject/AngularProject/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
-  apiAddress: 'http://localhost:64326'
+  apiAddress: 'http://localhost:64326/api',
+  cartName: 'mycart',
+  encKey: 'mysecretkey',
+  baseAddress: 'http://localhost:4200',
+  paymentGatewayUrl: 'https://test.payu.in/_payment',
+  paymentGatewaykey: 'gtKFFx',
+  paymentGatewaysalt: 'eCwWELxi',
 };
--- AngularProject/AngularProject/ClientApp/src/environments/environment.ts
+++ AngularProject/AngularProject/ClientApp/src/environments/environment.ts
@@ -4,7 +4,13 @@
 
 export const environment = {
   production: false,
-  apiAddress: 'http://localhost:64326'
+  apiAddress: 'http://localhost:64326/api',
+  cartName: 'mycart',
+  encKey: 'mysecretkey',
+  baseAddress: 'http://localhost:4200',
+  paymentGatewayUrl: 'https://test.payu.in/_payment',
+  paymentGatewaykey: 'gtKFFx',
+  paymentGatewaysalt: 'eCwWELxi',
 };
 
 /*
