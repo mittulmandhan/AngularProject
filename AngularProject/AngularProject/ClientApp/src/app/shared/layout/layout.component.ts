0fefb41cbc3d991546064ba62715eed5ae4ab874
mittulmandhan <conanmittul@gmail.com>
2019-09-22 01:49:32 +0530

    welcome message on default layout added

--- AngularProject/AngularProject/ClientApp/src/app/shared/layout/layout.component.ts
+++ AngularProject/AngularProject/ClientApp/src/app/shared/layout/layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserModel } from 'src/app/models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class LayoutComponent implements OnInit {
-
-  constructor() { }
+  user: UserModel;
+  constructor(private authService: AuthService, private router: Router) {
+    this.user = this.authService.user;
+  }
 
   ngOnInit() {
   }
-
+  logOut() {
+    this.authService.clearUserDetails();
+    // this.router.navigate(['/logout']);
+    window.location.href = '/logout';
+  }
 }
