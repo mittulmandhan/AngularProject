78eeebfdb99f04f184f493b7d5dbdf07f60b60e6
mittulmandhan <conanmittul@gmail.com>
2019-09-22 00:06:14 +0530

    component to show list of all products

--- AngularProject/AngularProject/ClientApp/src/app/Areas/admin/products/products.component.ts
+++ AngularProject/AngularProject/ClientApp/src/app/Areas/admin/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  products: Product[];
+  constructor(private productService: ProductService, ) { }
 
   ngOnInit() {
+    this.productService.GetProducts()
+      .subscribe(res => {
+        this.products = res;
+      });
   }
-
 }
