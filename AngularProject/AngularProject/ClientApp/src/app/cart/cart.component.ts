72d9bc11038ddb4ea08c9da45131f1b6e9c89a50
mittulmandhan <conanmittul@gmail.com>
2019-09-22 20:29:22 +0530

    checkout method added in cart component

--- AngularProject/AngularProject/ClientApp/src/app/cart/cart.component.ts
+++ AngularProject/AngularProject/ClientApp/src/app/cart/cart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { Cart } from '../models/cart';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { StoreService } from '../services/store.service';
 
 @Component({
   selector: 'app-cart',
@@ -9,10 +12,22 @@ import { Cart } from '../models/cart';
 })
 export class CartComponent implements OnInit {
 cart: Cart;
-  constructor(public cartService: CartService) { }
+
+  // tslint:disable-next-line:max-line-length
+  constructor(public cartService: CartService, private authService: AuthService, private storeService: StoreService, private router: Router) { }
 
   ngOnInit() {
     this.cart = this.cartService.getCart();
   }
-
+  chekout() {
+    if (this.authService.user.UserId) {
+      this.cart.UserId = this.authService.user.UserId;
+      this.storeService.SaveCart(this.cart).subscribe(res => {
+        this.cart.CartId = res;
+        this.cartService.checkoutPayUmoney(this.authService.user);
+      });
+    } else {
+      window.location.href = 'Account/login';
+    }
+  }
 }
