34c27e88a2e3477856af650eea6d2eea7488f8b7
mittulmandhan <conanmittul@gmail.com>
2019-09-24 00:53:24 +0530

    Product service dependency replaced with store dependencies

--- AngularProject/AngularProject/ClientApp/src/app/store/store.component.ts
+++ AngularProject/AngularProject/ClientApp/src/app/store/store.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
 import { CartService } from '../services/cart.service';
+import { StoreService } from '../services/store.service';
 
 @Component({
   selector: 'app-store',
@@ -10,10 +10,10 @@ import { CartService } from '../services/cart.service';
 })
 export class StoreComponent implements OnInit {
   products: Product[];
-  constructor(private productService: ProductService, public cartService: CartService) { }
+  constructor(private storeService: StoreService, public cartService: CartService) { }
 
   ngOnInit() {
-    this.productService.GetProducts().subscribe(res => {
+    this.storeService.GetProducts().subscribe(res => {
       this.products = res;
     });
   }
